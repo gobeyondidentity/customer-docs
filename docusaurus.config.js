cc62340c665b2cd03d4a054839e2cc9859eef059
Nelson Melo <nmelo.cu@gmail.com>
2022-10-07 12:37:14 -0400

    Some updates to config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,8 +16,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'gobeyondidentitybeyond', // Usually your GitHub org/user name.
+  projectName: 'customer-docs', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -58,7 +58,7 @@ const config = {
     ({
       colorMode: {
         defaultMode: 'dark',
-        disableSwitch: true,
+        disableSwitch: false,
         respectPrefersColorScheme: true,
       },
       navbar: {
