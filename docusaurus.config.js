f0f1b3115939780151e649a1aad712a53b3bbde3
Patricia McPhee <patricia.mcphee@beyondidentity.com>
2023-03-27 10:53:15 -0700

    remove topic

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,7 +3,7 @@
 
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-
+const baseUrl = process.env.BASE_URL ?? '/';
 
 
 /** @type {import('@docusaurus/types').Config} */
@@ -36,12 +36,13 @@ themes: [
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'throw',
   favicon: 'img/favicon.png',
-  trailingSlash: false,
+  trailingSlash: true,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'gobeyondidentitybeyond', // Usually your GitHub org/user name.
   projectName: 'customer-docs', // Usually your repo name.
+
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -173,10 +174,6 @@ customFields: {
                 to: '/blog', 
                 label: 'Release Notes 2'
               },
-              {
-                to: '/release-notes',
-                label: 'Release Notes 3',
-              }
             ],
           }, 
  
