43a972ef0ad0a290b885a8eb868e1d16c5e8bcb2
Patricia McPhee <patricia.mcphee@beyondidentity.com>
2023-05-17 09:22:10 -0700

    add release notes content

--- sidebars.js
+++ sidebars.js
@@ -9,10 +9,6 @@ const sidebars = {
       label: 'Release Notes',  
       collapsed: false, 
       collapsible: false,  
-/*      link: {
-        type: 'doc',
-        id: 'release-notes/release-notes-main',
-      }, */
       items: [ 
         'release-notes/v2-80-0',
         'release-notes/v2-79-0',
@@ -20,87 +16,13 @@ const sidebars = {
         'release-notes/v2-77-0',
         'release-notes/v2-76-0',
         'release-notes/v2-75-0',
-        'release-notes/v2-74-0',
-/*        {
-          type: 'category',
-          label: 'v2.73.0 • Jan 27 2023',
-          collapsed: false,
-          collapsible: false,   
-          link: {
-            type: 'doc',
-            id: 'release-notes/v2-73-0',
-          },      
-     
-              items: [ 
-                'release-notes/v2-73-1',
 
-              ],
-            },
-            {
-              type: 'category',
-              label: 'v2.72.0 • Jan 13 2023',
-              collapsed: false, 
-              collapsible: false,  
-              link: {
-                type: 'doc',
-                id: 'release-notes/v2-72-0',
-              },      
-         
-                  items: [ 
-                    'release-notes/v2-72-1',
-                    'release-notes/v2-72-2',
-                    'release-notes/v2-72-3',
-                  ],
-                },
-          
-        'release-notes/v2-71-0',
-        'release-notes/v2-70-0',
-        */
       ],
     },   
 
     'glossary',
 
   ],
-
-  /*
-  releaseNotesSidebar: [
-    {
-      type: 'category',
-      label: 'Release Notes',  
-      link: {
-        type: 'doc',
-        id: 'release-notes/release-notes-main',
-      },
-      collapsible: false,    
-      items: [ 
-        'release-notes/v2-77-0',
-        'release-notes/v2-76-0',
-        'release-notes/v2-75-0',
-      ],
-    },    
-  ],
-*/
-
-
-  /*
-  contributorSidebar: [
-    {
-      type: 'category',
-      label: 'Contributor's Guide',  
-      link: {
-        type: 'doc',
-        id: 'release-notes/release-notes-main',
-      },
-      collapsible: false,    
-      items: [ 
-        'release-notes/v2-77-0',
-        'release-notes/v2-76-0',
-        'release-notes/v2-75-0',
-      ],
-    },    
-  ],
-*/
 
 };
 
