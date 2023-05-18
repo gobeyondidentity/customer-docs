8005a546d04d7709b16d860f52010eeb85a1d005
Patricia McPhee <patricia.mcphee@beyondidentity.com>
2023-05-18 15:39:54 -0700

    fix dark mode for table rows

---
+++ src/theme/Redoc/ServerRedoc.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import clsx from 'clsx';
+import '../../global';
+import { Redoc as RedocComponent } from 'redoc';
+import { useSpec } from '../../utils/useSpec';
+import { ServerStyles } from './Styles';
+import './styles.css';
+function ServerRedoc(props) {
+    const { className, optionsOverrides, ...specProps } = props;
+    const { store, darkThemeOptions, lightThemeOptions, hasLogo } = useSpec(specProps, optionsOverrides);
+    return (<>
+      <ServerStyles specProps={specProps} lightThemeOptions={lightThemeOptions} darkThemeOptions={darkThemeOptions}/>
+      <div className={clsx([
+            'redocusaurus',
+            hasLogo && 'redocusaurus-has-logo',
+            className,
+        ])}>
+        <RedocComponent store={store}/>
+      </div>
+    </>);
+}
+export default ServerRedoc;
---
+++ src/theme/Redoc/Styles.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import '../../global';
+export function ServerStyles(_props) {
+    return <div className="redocusaurus-styles"></div>;
+}
