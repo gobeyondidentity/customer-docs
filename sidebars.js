2d86baba0a476e257ab71f55f2caedabf8e621ad
Patricia McPhee <patricia.mcphee@beyondidentity.com>
2023-06-14 08:49:07 -0700

    test redirects

--- sidebars.js
+++ sidebars.js
@@ -4,24 +4,7 @@
 const sidebars = {
   // By default, Docusaurus generates a sidebar from the docs folder structure
   secureWorkforceSidebar: [
-    {
-      type: 'category',
-      label: 'Release Notes',  
-      collapsed: false, 
-      collapsible: false,  
-      items: [ 
-        'release-notes/v2-81-0',
-        'release-notes/v2-80-0',
-        'release-notes/v2-79-0',
-        'release-notes/v2-78-0',
-        'release-notes/v2-77-0',
-        'release-notes/v2-76-0',
-        'release-notes/v2-75-0',
-
-      ],
-    },   
-
-    'glossary',
+    
 
   ],
 
