82653fa519b836eba866faf747ce84943247922a
Patricia McPhee <patricia.mcphee@beyondidentity.com>
2023-07-26 15:30:14 -0700

    update version banner in sidebar

--- src/theme/DocSidebarItems/index.js
+++ src/theme/DocSidebarItems/index.js
@@ -35,7 +35,7 @@ export default function DocSidebarItemsWrapper(props) {
           )}
         >
           <ThemeProvider theme={theme}>
-          <Chip label="Version: 2.83.0" href="#" size="small" className="sidebar-version" sx={{ fontWeight: 700}} />
+          <Chip label="Version: 2.84.0" href="#" size="small" className="sidebar-version" sx={{ fontWeight: 700}} />
           </ThemeProvider>
         </li>
       ) : null}
