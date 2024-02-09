9577e29ffbc67545536515514f8703688ca53c2a
shannon moore <shannon.moore@beyondidentity.com>
2024-02-09 08:31:32 -0600

    Revert "Moved query topics into device query"
    This reverts commit f3f3c0c7df50e8066eabc2768f8b47114a051d4d.

--- sidebars.js
+++ sidebars.js
@@ -37,7 +37,14 @@ const sidebars = {
       items: [
         "inventory/system-requirements",
         "inventory/device-inventory",
-        "inventory/device-query",
+        "inventory/analyze-devices",
+        "inventory/rename-device",
+        "inventory/rebind-device",
+        "inventory/delete-device",
+        "inventory/remove-passkey",
+        "inventory/view-query-status",
+        "inventory/run-device-query",
+        "inventory/view-query-results",
       ],
     },
     {
