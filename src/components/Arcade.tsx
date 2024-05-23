e65903d56a5f18a216a258c5127c98dc43f8bb39
shannon moore <shannon.moore@beyondidentity.com>
2024-05-23 11:49:36 -0500

    Updated D360 guide intro
    Includes link to free trial and arcade

--- src/components/Arcade.tsx
+++ src/components/Arcade.tsx
@@ -31,7 +31,8 @@ export enum Clip {
   GroupsAddGroupCallout = "ZOGqnheQpo9C4tAywQOm",
   D360QueryLibrary = "aEZVxkl5OLL3Uxod4pCZ",
   D360CreateQueryFromExisting = "D8RKeOQr4fTMQTVYrGsc",
-  CrowdStrikeIntegration = "XKmmOlte5oBTVAKjB874"
+  CrowdStrikeIntegration = "XKmmOlte5oBTVAKjB874",
+  Device360 = "z9hyF8cHLKTwBns6WQ57",
 }
 
 export const ClipPaddingBottom: { [key: string]: string } = {
@@ -55,6 +56,7 @@ export const ClipPaddingBottom: { [key: string]: string } = {
   [Clip.D360QueryLibrary]: "calc(48.96988906497623% + 41px)",
   [Clip.D360CreateQueryFromExisting]: "calc(51.38528138528139% + 41px)",
   [Clip.CrowdStrikeIntegration]: "calc(65.7042869641295% + 41px)",
+  [Clip.Device360]: "calc(68.21043165467626% + 41px)",
 };
 
 /**
